128f902756c3694a911801845bb52922290d8c85
jaspreet1388 <chha0038@algonquinlive.com>
2025-03-14 22:25:53 -0400

    Update JS.js

--- JS.js
+++ JS.js
@@ -63,11 +63,9 @@ class MenuButtonActions {
     this.menuitemNodes.forEach(function (item) {
 // TOUFIC'S COMMENT: Placeholder for the roving tabindex logic  
       if (item === newMenuitem) {
-
-        item.tabIndex = 0;
-
-        item.focus();       } else {         item.tabIndex = -1;       }
- ;)
+                          item.tabIndex = 0;
+                          item.focus();       } else {         item.tabIndex = -1;       }
+ 
     });
   }
 
